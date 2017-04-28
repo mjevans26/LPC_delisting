271c18c8398c6acf03b456dc1b595a8572f23179
mjevans26 <evans.michael26@gmail.com>
2017-04-28 17:36:35 -0400

    improved use of LDA, new ag detection testing data

--- processing.R
+++ processing.R
@@ -19,6 +19,7 @@ pcc(cde[,3:6], cde[,1], nboot = 0)
 src(cde[,3:6], cde[,2], nboot = 0)
 
 rawcd <- read.csv("SRange_chngData.csv", header = TRUE, sep = ",")
+rawcdN <- read.csv("NRange_chngData.csv", header = TRUE, sep = ",")
 
 plot_ly()%>%
   add_trace(data = rawcd[rawcd$Index == 1,], x = ~ mode, y = ~ndsi_z, type = "box")%>%
@@ -26,11 +27,12 @@ plot_ly()%>%
   layout(barmode = "group")
   
 rawcd_stats <- read.table("SRange_ChangeStats.txt", header = TRUE, sep = "\t")
+rawcd_statsN <- read.table("NRange_ChangeStats.txt", header = TRUE, sep = "\t")
 
-rawcd$cv_z <- (rawcd$cv - rawcd_stats$Min[rawcd_stats$Metric == "cv"])/rawcd_stats$SD[rawcd_stats$Metric == "cv"]
-rawcd$rcv_z <- (rawcd$rcvmax - rawcd_stats$Min[rawcd_stats$Metric == "rcv"])/rawcd_stats$SD[rawcd_stats$Metric == "rcv"]
-rawcd$ndvi_z <- (rawcd$ndvi - rawcd_stats$Mean[rawcd_stats$Metric == "ndvi"])/rawcd_stats$SD[rawcd_stats$Metric == "ndvi"]
-rawcd$ndsi_z <- (rawcd$ndsi - rawcd_stats$Mean[rawcd_stats$Metric == "ndsi"])/rawcd_stats$SD[rawcd_stats$Metric == "ndsi"]
+rawcdN$cv_z <- (rawcdN$cv - rawcd_statsN$Min[rawcd_statsN$Metric == "cv"])/rawcd_statsN$SD[rawcd_statsN$Metric == "cv"]
+rawcdN$rcv_z <- (rawcdN$rcvmax - rawcd_statsN$Min[rawcd_statsN$Metric == "rcv"])/rawcd_statsN$SD[rawcd_statsN$Metric == "rcv"]
+rawcdN$ndvi_z <- (rawcdN$ndvi - rawcd_statsN$Mean[rawcd_statsN$Metric == "ndvi"])/rawcd_statsN$SD[rawcd_statsN$Metric == "ndvi"]
+rawcdN$ndsi_z <- (rawcdN$ndsi - rawcd_statsN$Mean[rawcd_statsN$Metric == "ndsi"])/rawcd_statsN$SD[rawcd_statsN$Metric == "ndsi"]
 
 ct <- ctree(Index~cv_z+rcv_z+ndvi_z+ndsi_z+mode, data = rawcd[rawcd$mode == 82,], controls = ctree_control(mincriterion = 0.95))
 
@@ -41,5 +43,15 @@ scrub_pred <- predict(scrub, type = "prob")
 ldahist(data = scrub_pred$x[,1], g = scrub_pred$class)
 scrub_out <- data.frame("predict" = scrub_pred$class, "LD1" = scrub_pred$x[,1], "p0" = scrub_pred$posterior[,1], "p1" = scrub_pred$posterior[,2], "observed" = rawcd$Index)
 scrub_out$predict <- as.numeric(as.character(scrub_out$predict))
-scrub_roc <- roc(response = scrub_out$observed, predictor = scrub_out$p1)
-plot_ly(data = as.data.frame(scrub_roc[2:4]), y = ~sensitivities, x = ~ specificities, type = "scatter", text = ~paste("Thresh:", thresholds), hoverinfo = text)
+scrub_roc <- roc(response = scrub_out$observed, predictor = scrub_out$LD1)
+plot_ly(data = as.data.frame(scrub_roc[2:4]), y = ~sensitivities, x = ~ 1- specificities, type = "scatter", text = ~paste("Thresh:", thresholds), hoverinfo = text)
+
+N <- lda(Index ~ cv_z + rcv_z + ndvi_z + ndsi_z, data = rawcdN)
+N_pred <- predict(N, type = "prob")
+ldahist(data = N_pred$x[,1], g = N_pred$class)
+N_out <- data.frame("predict" = N_pred$class, "LD1" = N_pred$x[,1], "p0" = N_pred$posterior[,1], "p1" = N_pred$posterior[,2], "observed" = rawcdN$Index)
+N_out$predict <- as.numeric(as.character(N_out$predict))
+N_roc <- roc(response = N_out$observed, predictor = N_out$LD1)
+plot_ly(data = as.data.frame(N_roc[2:4]), y = ~sensitivities, x = ~ 1 - specificities, type = "scatter", text = ~paste("Thresh:", thresholds), hoverinfo = text)
+
+LSag <- read.csv(file = "KS_ag_raw.csv", header = TRUE)
